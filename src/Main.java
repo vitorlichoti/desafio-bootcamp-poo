ed5574c8cc58884b823e1973958ccea23ea9f012
Vitor Lichoti <vitorlichoti@gmail.com>
2023-05-14 12:18:43 -0400

    final: interligando as classes do dominio

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 import java.time.LocalDate;
 
 import br.com.dio.desafio.domain.Curso;
+import br.com.dio.desafio.domain.Dev;
 import br.com.dio.desafio.domain.Mentoria;
-import br.com.dio.desafio.domain.Conteudo;
+import br.com.dio.desafio.domain.Bootcamp;
 
 public class Main {
     public static void main(String[] args) {
@@ -20,6 +21,24 @@ public class Main {
         mentoria1.setTitulo("mentoria de Java");
         mentoria1.setDescricao("Mentoria para o curso de Java");
         mentoria1.setData(LocalDate.now());
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria1);
+
+        Dev devVitor = new Dev();
+        devVitor.setNome("Vitor");
+        devVitor.inscreverBootCamp(bootcamp);
+        System.out.println("Conteúdos Inscritos de Vitor:" + devVitor.getConteudosInscritos());
+        devVitor.progredir();
+        devVitor.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos de Vitor:" + devVitor.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos de Vitor:" + devVitor.getConteudosConcluidos());
+        System.out.println("XP:" + devVitor.calcularTotalXp());
 
         System.out.println(curso1);
         System.out.println(curso2);
